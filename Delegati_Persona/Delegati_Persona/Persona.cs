f07a65b7e321c03bbf6ffe30850cfce592c665bb
Nicolò Coltro <coltro.school@gmail.com>
2021-10-03 09:14:00 +0200

    Delegati Persona

---
+++ Delegati_Persona/Delegati_Persona/Persona.cs
@@ -0,0 +1,11 @@
+namespace Delegati_Persona {
+    public class Persona {
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona(string nome, int eta) {
+            Nome = nome;
+            Eta = eta;
+        }
+    }
+}
---
+++ Delegati_Persona/Delegati_Persona/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Delegati_Persona {
+    public delegate bool CheckEta(int eta); // Delegato per chiamata CheckEta
+
+    public class Program {
+        public static bool CheckEta(Persona p, CheckEta check) {
+            return check(p.Eta);
+        }
+
+        public static bool CheckAdulto(int eta) { // Check per Adulto
+            if (eta >= 18 && eta < 65)
+                return true;
+            return false;
+        }
+
+        public static bool CheckRagazzo(int eta) { // Check per Ragazzo
+            if (eta < 18)
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args) {
+            Persona p1 = new Persona("Nicolò", 18);
+            Console.WriteLine("Check Ragazzo: " + CheckEta(p1, CheckRagazzo));
+            Console.WriteLine("Check Adulto: " + CheckEta(p1, CheckAdulto));
+        }
+    }
+}
