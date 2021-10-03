79a77401f92823ff7820e27f87be5493840ddb90
Nicolò Coltro <coltro.school@gmail.com>
2021-10-03 10:05:18 +0200

    Delegati Persona List<>

--- Delegati_Persona/Delegati_Persona/Program.cs
+++ Delegati_Persona/Delegati_Persona/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Delegati_Persona {
     public delegate bool CheckEta(int eta); // Delegato per chiamata CheckEta
@@ -8,22 +9,37 @@ namespace Delegati_Persona {
             return check(p.Eta);
         }
 
-        public static bool CheckAdulto(int eta) { // Check per Adulto
-            if (eta >= 18 && eta < 65)
-                return true;
-            return false;
-        }
-
         public static bool CheckRagazzo(int eta) { // Check per Ragazzo
             if (eta < 18)
                 return true;
             return false;
         }
-
+        
+        public static bool CheckAdulto(int eta) { // Check per Adulto
+            if (eta >= 18 && eta < 65)
+                return true;
+            return false;
+        }
+        
+        public static bool CheckAnziano(int eta) { // Check per Ragazzo
+            if (eta >= 65)
+                return true;
+            return false;
+        }
+        
         static void Main(string[] args) {
-            Persona p1 = new Persona("Nicolò", 18);
-            Console.WriteLine("Check Ragazzo: " + CheckEta(p1, CheckRagazzo));
-            Console.WriteLine("Check Adulto: " + CheckEta(p1, CheckAdulto));
+
+            List<Persona> persone = new List<Persona>();
+            persone.Add(new Persona("Nicolò", 18));
+            persone.Add(new Persona("Silvia", 50));
+            persone.Add(new Persona("Matteo", 17));
+            persone.Add(new Persona("Franco", 78));
+
+            Console.WriteLine("Nome\t\tRagazzo\tAdulto\tAnziano");
+
+            persone.ForEach(x => 
+                    Console.WriteLine(x.Nome + " (" + x.Eta + ")\t" + CheckEta(x, CheckRagazzo) + "\t" + CheckEta(x, CheckAdulto) + "\t" + CheckEta(x, CheckAnziano))
+                    );
         }
     }
 }
