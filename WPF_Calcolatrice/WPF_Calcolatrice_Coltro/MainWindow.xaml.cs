555517ea6b137dcac7eaf2c55dd76d8f30bd11cb
Nicolò Coltro <coltro.school@gmail.com>
2021-11-10 14:30:47 +0100

    repo update

---
+++ WPF_Calcolatrice/WPF_Calcolatrice_Coltro/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_Calcolatrice
+{
+    public partial class MainWindow : Window
+    {
+        private string buffer = "";
+        private bool ris = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static string eval(string s)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("expression", typeof(string), s);
+                DataRow dr = dt.NewRow();
+                dt.Rows.Add(dr);
+                return (string)dr["expression"];
+            }
+            catch
+            {
+                return "Err";
+            }
+        }
+
+        public string key2Char(Key k)
+        {
+            switch (k)
+            {
+                case Key.Add:
+                    return "+";
+                case Key.Subtract:
+                    return "-";
+                case Key.Multiply:
+                    return "*";
+                case Key.Divide:
+                    return "/";
+                case Key.Escape:
+                    return "C";
+                case Key.Return:
+                    return "=";
+                case Key.Back:
+                    return "B";
+                default:
+                    string ks = k.ToString();
+
+                    if (ks[ks.Length - 1] >= 48 && ks[ks.Length - 1] <= 57)
+                        return ks[ks.Length - 1].ToString(); // Numbers
+                    else
+                        return ""; // Other Keys
+            }
+        }
+
+        private void ButtonHandler(object s, RoutedEventArgs e)
+        {
+            uguale.Focus();
+            send2Display((s as Button).Content.ToString()); // Check pressed button
+        }
+
+        private void KeyboardHandler(object s, KeyEventArgs e)
+        {
+            uguale.Focus();
+            send2Display(key2Char(e.Key)); // Check pressed key (toChar)
+        }
+
+        private void send2Display(string s)
+        {
+            if (s == "C" || ris)
+            {
+                ris = false; // Reset result flag
+                buffer = ""; // Empty buffer
+            }
+
+            if (s == "=")
+            {
+                buffer = eval(buffer).ToString(); // Calc expression
+                ris = true; // Enable result flag
+            }
+            else if (s == "B" && buffer.Length > 0)
+                buffer = buffer.Substring(0, buffer.Length - 1); // Remove last char
+            else if (s != "C" && s != "B")
+                buffer += s; // Stack in buffer
+
+            display.Content = buffer; // Send buffer to display (label)
+        }
+    }
+}
---
+++ WPF_CodedGrid/WPF_CodedGrid/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CodedGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Grid lastGrid;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonHandler(object sender, RoutedEventArgs e)
+        {
+            Grid g = new Grid(); // New grid
+            g.ShowGridLines = true;
+
+            int colCount = int.Parse(colNum.Text.ToString());
+            int rowCount = int.Parse(rowNum.Text.ToString());
+
+            for (int i = 0; i < colCount; i++)
+                g.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int i = 0; i < rowCount; i++)
+                g.RowDefinitions.Add(new RowDefinition());
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++)
+                {
+                    Label l = new Label();
+                    l.Content = "Cella " + j + ";" + i;
+                    g.Children.Add(l);
+
+                    Grid.SetRow(l, i);
+                    Grid.SetColumn(l, j);
+                }
+            }
+
+            if (lastGrid != null) // If lastGrid exists, remove
+                mainGrid.Children.Remove(lastGrid);
+
+            mainGrid.Children.Add(g); // Append to main grid
+            lastGrid = g; // Update last grid
+
+            Grid.SetRow(g, 1); // Set new grid position in main grid
+        }
+    }
+}
