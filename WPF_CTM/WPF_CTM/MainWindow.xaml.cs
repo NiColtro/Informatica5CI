fee633dabfb7cacaa874bc42d5451c4fcc800344
Nicolò Coltro <coltro.school@gmail.com>
2021-11-16 21:55:23 +0100

    ctm update

--- WPF_CTM/WPF_CTM/MainWindow.xaml.cs
+++ WPF_CTM/WPF_CTM/MainWindow.xaml.cs
@@ -6,13 +6,8 @@ using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace WPF_CTM {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
     public partial class MainWindow : Window {
-        DispatcherTimer timer = new DispatcherTimer(); // Timer
+        DispatcherTimer timer; // Timer
         Random r = new Random(); // Random obj generator
         int clicks = 0; // Clicks counter
 
@@ -20,6 +15,7 @@ namespace WPF_CTM {
         List<Button> validClicks = new List<Button>(); // Valid clicks button list
 
         private void timerSetup() {
+            timer = new DispatcherTimer();
             timer.Tick += new EventHandler(timerHandler); // Execute at every timer tick
             timer.Interval = TimeSpan.FromMilliseconds(500); // Start interval between ticks
             timer.Start(); // Start timer
@@ -34,7 +30,7 @@ namespace WPF_CTM {
             do randomIndex = r.Next(9); // Generate a random index...
             while (validClicks.Contains(gameGrid.Children[randomIndex] as Button)); // ...while not already picked
 
-            (gameGrid.Children[randomIndex] as Button).Background = Brushes.Yellow; // Identify current button
+            (gameGrid.Children[randomIndex] as Button).Background = Brushes.SandyBrown; // Identify current button
             lastButton = (Button)gameGrid.Children[randomIndex]; // Update lastButton
         }
 
@@ -67,13 +63,14 @@ namespace WPF_CTM {
             Clicks.Content = "Clicks: " + clicks;
 
             if (s == lastButton) { // Clicked button was the correct one
-                updateTimer(timer.Interval.TotalMilliseconds * 0.70); // Decrese change interval
+                updateTimer(timer.Interval.TotalMilliseconds * 0.95); // Decrese change interval
                 validClicks.Add(s); // Add current button to valid list
-                s.Background = Brushes.Green;
+                s.Background = (Brush) new BrushConverter().ConvertFrom("#faee70");
+
                 Points.Content = "Punti: " + validClicks.Count;
             }
 
-            if (validClicks.Count >= 3) { // WIN!
+            if (validClicks.Count >= 9) { // WIN!
                 timer.Stop();
                 WinMsg.Visibility = Visibility.Visible;
             }
